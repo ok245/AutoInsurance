b58bdfcd42dc7d4b56ec941cabdd860d0f8b3a93
ok245 <omkarkadam245@gmail.com>
2025-05-11 12:10:58 +0530

    service that generate quotes for auto insurance

---
+++ src/main/java/com/app/AutoInsurance/AutoInsuranceApplication.java
@@ -0,0 +1,14 @@
+package com.app.AutoInsurance;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoInsuranceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoInsuranceApplication.class, args);
+	}
+
+}
